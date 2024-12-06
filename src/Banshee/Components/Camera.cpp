64afe0b6e849af18ed00fddd775c2fc7b74101d2
elsantiF <santiagofarall@gmail.com>
2024-12-06 00:10:52 -0300

    [Banshee] Better Camera constructor

--- src/Banshee/Components/Camera.cpp
+++ src/Banshee/Components/Camera.cpp
@@ -1,11 +1,8 @@
 #include "Camera.h"
 
 namespace Banshee {
-    Camera::Camera(const f32 fov, const f32 aspect, const f32 near, const f32 far) {
-        m_Fov = fov;
-        m_Aspect = aspect;
-        m_Near = near;
-        m_Far = far;
+    Camera::Camera(const f32 fov, const f32 aspect, const f32 near, const f32 far)
+        : m_Fov{fov}, m_Aspect{aspect}, m_Near{near}, m_Far{far}, m_Transform{} {
 
         m_ProjectionMatrix = glm::perspective(glm::radians(m_Fov), m_Aspect, m_Near, m_Far);
 
